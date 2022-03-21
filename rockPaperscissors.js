430e807bada3b08e36bf09e37afb595e54270db3
maxamed1st <maxamed1st@icloud.com>
2022-03-21 12:50:38 +0100

    Desig the func game() to enable 5 round game
    func game() keeps score of every round and even evaluates if a game is a tie thus excluding that round.
    
    add func asserValidChoice() to asses if players choice is valid!

--- rockPaperscissors.js
+++ rockPaperscissors.js
@@ -15,14 +15,20 @@ function roundPlay(computerSelection, playerSelection) {
     Compare the selections to determin a winner.
     Return players win/lost status as string
     */
-   playerSelection = playerSelection.toLowerCase()
-   if (playerSelection == computerSelection) return "This round is even"
-   else if (playerSelection=="rock" && computerSelection=="scissors" || 
+   playerSelection = playerSelection.toLowerCase().trim()
+   if (playerSelection=="rock" && computerSelection=="scissors" || 
    playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper") {
        return "Player 1 is the winner of this round"
    } else {
-       return "The computer is the winner of this round"
+       return "Computer took home this round"
    }
+}
+function assertValidChoice(playerSelection) {
+    //Assert valid choices for the player
+    choices = ["rock", "paper", "scissors"]
+    playerSelection = playerSelection.toLowerCase().trim()
+    if (choices.indexOf(playerSelection) >= 0) return true
+    else return false
 }
 function game() {
     /*Play 5 rounds of the game by calling roundPlay 5x.
@@ -31,10 +37,44 @@ function game() {
     */
    score = [0, 0]
    for (let i=0; i<5; i++) {
-       computerSelection = computerPlay()
-       playerSelection = prompt('Select your weapon:')
-       console.log(roundPlay(computerSelection, playerSelection))
-   }
-}
+       computerSelection = computerPlay();
+       playerSelection = prompt('Select your weapon:');
 
-game()
+       //make sure that playerSelection and computerSelection are not the same
+       while (computerSelection == playerSelection.trim()) {
+           console.log('This round is even. Go for another round!')
+           computerSelection = computerPlay();
+           playerSelection = prompt('Select your weapon agian')
+       }
+       
+       //Assert valid player choice
+       choiceValidity = assertValidChoice(playerSelection)
+       while (choiceValidity===false) {
+           playerSelection = prompt('You have chosen an insufficient weapon for this war. \
+           Remember this is a game of rock, paper scissors and choose again!');
+           choiceValidity = assertValidChoice(playerSelection)
+        }
+
+       //determend winner and update score
+       winner = roundPlay(computerSelection, playerSelection);
+       if (winner[0] == "P") {
+           score[0] += 1;
+           console.log(`score: ${score} winner: ${winner}`)
+       } else if (winner[0] == "C") {
+           score[1] += 1;
+           console.log(`score: ${score} winner: ${winner}`)
+       } else {
+           console.log(`The fuck just happen?? score: ${score} winner: ${winner}`)
+       }
+
+       console.log(`END of round ${i+1}`)
+    }
+
+    //present result
+   if (score[0] >= score[1]) {
+       return `player 1 is the winner of this game! with total of ${score[0]} wins against ${score[1]} loss`
+    } else {
+        return `Computer took hame the game this time with ${score[1]} wins and ${score[0]} loss `}
+    }
+
+console.log(game())
