9f260c63ca1f89b1eb3895b5684c92d26a89c9ed
maxamed1st <maxamed1st@icloud.com>
2022-03-21 13:17:59 +0100

    Present results of the rounds with more context

--- rockPaperscissors.js
+++ rockPaperscissors.js
@@ -18,9 +18,9 @@ function roundPlay(computerSelection, playerSelection) {
    playerSelection = playerSelection.toLowerCase().trim()
    if (playerSelection=="rock" && computerSelection=="scissors" || 
    playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper") {
-       return "Player 1 is the winner of this round"
+       return `Player is the winner of this round \n${playerSelection} beats ${computerSelection}`
    } else {
-       return "Computer took home this round"
+       return `Computer is the winner of this round \n${computerSelection} beats ${playerSelection}`
    }
 }
 function assertValidChoice(playerSelection) {
@@ -35,9 +35,8 @@ function selection() {
     playerSelection = prompt('Select your weapon:');
     //make sure that playerSelection and computerSelection are not the same
     while (computerSelection == playerSelection.trim()) {
-        console.log('This round is even. Go for another round!')
         computerSelection = computerPlay();
-        playerSelection = prompt('Select your weapon agian')
+        playerSelection = prompt('This round is even. Go for another round!')
     }
 }
 function game() {
@@ -62,22 +61,19 @@ function game() {
        winner = roundPlay(computerSelection, playerSelection);
        if (winner[0] == "P") {
            score[0] += 1;
-           console.log(`score: ${score} winner: ${winner}`)
        } else if (winner[0] == "C") {
            score[1] += 1;
-           console.log(`score: ${score} winner: ${winner}`)
        } else {
            console.log(`The fuck just happen?? score: ${score} winner: ${winner}`)
        }
-
-       console.log(`END of round ${i+1}`)
+       console.log(`${winner} \nscore: Player: ${score[0]} computer: ${score[1]} `)
     }
 
     //present result
    if (score[0] >= score[1]) {
        return `player 1 is the winner of this game! with total of ${score[0]} wins against ${score[1]} loss`
     } else {
-        return `Computer took hame the game this time with ${score[1]} wins and ${score[0]} loss `}
+        return `Computer took home the game with ${score[1]} wins and ${score[0]} loss `}
     }
 
 console.log(game())
